c12a682b3d130d91b87932b5304bf7bb391b0677
JoeyShapiro <joeyashapiro@gmail.com>
2025-06-13 21:18:06 -0500

    checkpoint: cleanup

--- src/main.go
+++ src/main.go
@@ -1,8 +1,5 @@
 package main
 
-// An example Bubble Tea server. This will put an ssh session into alt screen
-// and continually print up to date terminal information.
-
 import (
 	"context"
 	"errors"
@@ -81,31 +78,12 @@ func main() {
 	}
 }
 
-// You can wire any Bubble Tea model up to the middleware with a function that
-// handles the incoming ssh.Session. Here we just grab the terminal info and
-// pass it to the new model. You can also return tea.ProgramOptions (such as
-// tea.WithAltScreen) on a session by session basis.
 func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 	// This should never fail, as we are using the activeterm middleware.
 	pty, _, _ := s.Pty()
 
-	// When running a Bubble Tea app over SSH, you shouldn't use the default
-	// lipgloss.NewStyle function.
-	// That function will use the color profile from the os.Stdin, which is the
-	// server, not the client.
-	// We provide a MakeRenderer function in the bubbletea middleware package,
-	// so you can easily get the correct renderer for the current session, and
-	// use it to create the styles.
-	// The recommended way to use these styles is to then pass them down to
-	// your Bubble Tea model.
 	renderer := bubbletea.MakeRenderer(s)
-	txtStyle := renderer.NewStyle().Foreground(lipgloss.Color("10"))
-	quitStyle := renderer.NewStyle().Foreground(lipgloss.Color("8"))
-
-	bg := "light"
-	if renderer.HasDarkBackground() {
-		bg = "dark"
-	}
+	txtStyle := renderer.NewStyle()
 
 	var piece Cell
 	switch game.Players {
@@ -119,16 +97,13 @@ func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 	}
 
 	m := model{
-		term:      pty.Term,
-		profile:   renderer.ColorProfile().Name(),
-		width:     pty.Window.Width,
-		height:    pty.Window.Height,
-		bg:        bg,
-		txtStyle:  txtStyle,
-		quitStyle: quitStyle,
-		Player:    piece,
-		Conn:      make(chan tea.Msg, 1),
-		Id:        game.Players,
+		txtStyle: txtStyle,
+		term:     pty.Term,
+		width:    pty.Window.Width,
+		height:   pty.Window.Height,
+		Player:   piece,
+		Conn:     make(chan tea.Msg, 1),
+		Id:       game.Players,
 	}
 
 	game.Players++
@@ -139,17 +114,14 @@ func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 
 // Just a generic tea.Model to demo terminal information of ssh.
 type model struct {
-	term      string
-	profile   string
-	width     int
-	height    int
-	bg        string
-	txtStyle  lipgloss.Style
-	quitStyle lipgloss.Style
-	GameId    string
-	Player    Cell
-	Conn      chan tea.Msg
-	Id        int
+	term     string
+	width    int
+	height   int
+	txtStyle lipgloss.Style
+	Id       int
+	GameId   string
+	Player   Cell
+	Conn     chan tea.Msg
 }
 
 func listenCmd(m model) tea.Cmd {
@@ -185,13 +157,6 @@ func NewGame(id string) Game {
 		Conn:          make(chan tea.Msg, 3),
 	}
 }
-
-// could maybe have a history of all moves.
-// but i would need to compute every turn, and check for captures, etc.
-// and i would need the board on the frontend anyway
-// only need to compute the next turn, but still would need the board
-// doesnt save me, and might use a lot of memory
-// just dump it in the db
 
 const BOARD_SIZE = 9 // Go board is 19x19
 // 13 9
